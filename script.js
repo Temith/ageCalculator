de36d53ff8f1ff94c431c6f2750dc0efbaa68ddc
Akanbi Mariam <akanbitilayo17@gmail.com>
2023-04-27 05:55:16 +0100

    first version

---
+++ script.js
@@ -0,0 +1,11 @@
+const dayInput = document.getElementById("day");
+const monthInput = document.getElementById("month");
+const yearInput = document.getElementById("year");
+
+const dayOutput = document.getElementById("DD");
+const monthOutput = document.getElementById("MM");
+const yearOutput = document.getElementById("YY");
+
+const form = document.querySelector("form");
+const input = document.querySelector("block");
+const inputs = document.querySelector("children");
